f8a23e3bd514342e9a39ee7744b9c01670aa72d3
Jiani Cao <jianicakes@gmail.com>
2022-01-13 17:26:09 -0800

    function which returns first key of object which matches the one specified in callback

---
+++ findKey.js
@@ -0,0 +1,38 @@
+//TESTER FUNCTION
+const assertEqual = function(actual, expected) {
+  actual === expected
+  ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//FUNCTION WHICH RETURNS FIRST KEY OF OBJECT WHICH MATCHES THE ONE SPECIFIED IN CALLBACK
+const findKey = function(object, callback) {
+
+  for (const key in object) {
+    if (callback(object[key])) return key;
+  }
+  return undefined;
+}
+
+//TEST CASES
+const testResult = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(testResult, "noma");
+
+const anotherTest = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4)
+
+assertEqual(anotherTest, undefined);

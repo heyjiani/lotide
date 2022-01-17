4898b7fb1c1243642c4fb771aade23814197bd0e
Jiani Cao <jianicakes@gmail.com>
2022-01-16 17:48:29 -0800

    added recursion to compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,15 +7,24 @@ const assertEqual = function(actual, expected) {
 
 //FUNCTION FOR COMPARING ARRAYS FOR PERFECT MATCH
 const eqArrays = (arrayA, arrayB) => {
+  let result = true;
   if (arrayA.length !== arrayB.length) return false;
 
   for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) return false;
+    if (!Array.isArray(arrayA[i])) {
+      if (arrayA[i] !== arrayB[i]) return false;
+    }
+
+    if (Array.isArray(arrayA[i])) result = eqArrays(arrayA[i], arrayB[i]);
   }
-  return true;
+  return result;
 };
 
 //TEST CASES
 console.log(assertEqual(eqArrays([2,1,3],[1,2,3]), false));
 console.log(assertEqual(eqArrays([1,2,3],[1,2,3]), true));
 console.log(assertEqual(eqArrays([2,1],[1,2,3]), false));
+
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true));
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false));
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false));

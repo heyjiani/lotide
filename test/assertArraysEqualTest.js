f2a80b06a6ce5a0682bdf64328afaa295187e8a4
Jiani Cao <jianicakes@gmail.com>
2022-01-17 17:07:08 -0800

    added README & some descriptions

--- test/assertArraysEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertArraysEqual = require('../assertArraysEqual');
-
-// //TEST CASES
-// assertArraysEqual([1, 2, 3],[1, 2, 3]); //TRUE
-// assertArraysEqual([1, 3, 2],[1, 2, 3]); //FALSE
-// assertArraysEqual([1, 2, 3],[1, 2]); //FALSE
